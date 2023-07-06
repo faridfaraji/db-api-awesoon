0e8d95f942ec48a14d3eec0347d9f4d725082a21
farid <farid.faraji45@gmail.com>
2023-07-06 19:31:39 -0400

    fix

--- awesoon/api/util.py
+++ awesoon/api/util.py
@@ -6,15 +6,17 @@ def add_date_search_params(parser):
     parser.add_argument(
         "start_datetime",
         type=inputs.datetime_from_iso8601,
+        default=(datetime.utcnow()-timedelta(days=1)).isoformat(),
         required=False,
-        default=lambda: datetime.utcnow() - timedelta(days=1),
         help="Start date for filtering requested conversations",
+        location="values",
     )
     parser.add_argument(
         "end_datetime",
         type=inputs.datetime_from_iso8601,
         required=False,
-        default=lambda: datetime.utcnow(),
+        default=datetime.utcnow().isoformat(),
         help="End date for filtering requested conversations",
+        location="values",
     )
     return parser

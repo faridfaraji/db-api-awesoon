b55ba11329b1fe5dc6ea428e271871f9181b5952
Sam Churchill <meadowschurchill@gmail.com>
2023-07-06 15:50:54 -0700

    Update awesoon/api/util.py
    Co-authored-by: Farid Faraji <farid.faraji@alumni.ubc.ca>

--- awesoon/api/util.py
+++ awesoon/api/util.py
@@ -1,4 +1,5 @@
 from flask_restx import inputs
+from datetime import datetime, timedelta
 
 
 def add_date_search_params(parser):
@@ -6,12 +7,14 @@ def add_date_search_params(parser):
         "start_datetime",
         type=inputs.datetime_from_iso8601,
         required=False,
+        default=datetime.Now() - timedelta(days=1),
         help="Start date for filtering requested conversations",
     )
     parser.add_argument(
         "end_datetime",
         type=inputs.datetime_from_iso8601,
         required=False,
+        default=datetime.Now(),
         help="End date for filtering requested conversations",
     )
     return parser
